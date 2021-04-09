1576bc4f9100698268a38e98b8160876b4e27697
suaram95 <aram/*java2020>
2021-04-09 19:00:45 +0400

    init commit

---
+++ src/test/java/com/example/onlineStoreExample/OnlineStoreExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineStoreExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
